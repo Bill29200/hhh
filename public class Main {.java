daad6a99d89edd9cd6ffb2aafb2f309c310f9e0c
Bill29200 <nabil.kadjouh@gmail.com>
2024-09-13 17:49:30 +0200

    c'est mon premier programme java

---
+++ public class Main {.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args)
+  {
+    System.out.println("l'ensemble des argument sont :");
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+    }
+
+  }
+}
